2f454c80bde24e9498890a1b938064624d3516ba
admin <mounkaila144@gmail.com>
2024-10-24 18:49:27 +0100

    commit

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -18,24 +18,34 @@ class LivreController extends AbstractController
     {
         try {
             // Récupérer les données du formulaire
-            $livre = $request->request->get('livre');
             $firstName = $request->request->get('first_name');
             $lastName = $request->request->get('last_name');
             $phoneNumber = $request->request->get('phone_number');
+            $pack = $request->request->get('pack');
+            $addPass = $request->request->get('add_pass');
+            $promoteur = $request->request->get('promoteur'); // Récupérer le promoteur depuis le formulaire
+
 
             // Construire le message à envoyer via Telegram
             $bookIcon = "📚";
             $phoneIcon = "📞";
+            $passMessage = $addPass ? " + 1 PASS ajouté pour 10 000F" : "";
+            $promoteurMessage = $promoteur ? "Envoyé par le promoteur: <b>$promoteur</b>" : "Aucun promoteur";
+
 
             $message = sprintf(
-                "%s Commande de livre: <b>%s</b>\n\n👤 Prénom: <b>%s</b>\n👤 Nom: <b>%s</b>\n%s Téléphone: <a href='tel:%s'>%s</a>",
+                "%s Commande de pack: <b>%s</b>\n%s\n👤 Prénom: <b>%s</b>\n👤 Nom: <b>%s</b>\n%s Téléphone: <a href='tel:%s'>%s</a>%s",
                 $bookIcon,
-                htmlspecialchars($livre),
+                htmlspecialchars($pack),
+                $passMessage,
                 htmlspecialchars($firstName),
                 htmlspecialchars($lastName),
                 $phoneIcon,
                 $phoneNumber,
-                $phoneNumber
+                $phoneNumber,
+                $passMessage,
+                $promoteurMessage
+
             );
 
             // Envoyer le message via l'API Telegram
@@ -56,6 +66,7 @@ class LivreController extends AbstractController
 
         return new JsonResponse($response);
     }
+
 
 
     private function sendMessageToTelegram(string $message)
@@ -90,17 +101,25 @@ class LivreController extends AbstractController
 
 
     #[Route('/le-leadership-de-kirikou', name: 'app_kirikou')]
-    public function kirikou(): Response
+    public function kirikou(Request $request): Response
     {
+        $promoteur = $request->query->get('promoteur'); // Récupérer le promoteur depuis l'URL
+
         return $this->render('livre/kirikou.html.twig', [
             'controller_name' => 'LivreController',
+            'promoteur' => $promoteur // Passer le promoteur à la vue
+
         ]);
     }
      #[Route('/le-pouvoir-de-se-reveiller-tot', name: 'app_pouvoir')]
-    public function pouvoir(): Response
+    public function pouvoir(Request $request): Response
     {
+        $promoteur = $request->query->get('promoteur'); // Récupérer le promoteur depuis l'URL
+
         return $this->render('livre/pouvoir.html.twig', [
             'controller_name' => 'LivreController',
+            'promoteur' => $promoteur // Passer le promoteur à la vue
+
         ]);
     }
 }
